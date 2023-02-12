f01a8846132197a7d7a268667c929f48ba660f34
Iryna Sydoruk <iriss2288@gmail.com>
2023-02-12 02:22:02 +0200

    Logger&Migration

--- src/main/java/com/sydoruk/service/OrderHibernateService.java
+++ src/main/java/com/sydoruk/service/OrderHibernateService.java
@@ -4,6 +4,8 @@ import com.sydoruk.model.CarHibernate;
 import com.sydoruk.model.OrderHibernate;
 import com.sydoruk.model.Type;
 import com.sydoruk.repository.OrderHibernateRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +13,8 @@ import java.util.Optional;
 
 public class OrderHibernateService {
     private final OrderHibernateRepository repository;
-
     private final CarHibernateService service;
+    final static Logger logger = LoggerFactory.getLogger(OrderHibernateService.class);
 
     public OrderHibernateService(final  OrderHibernateRepository repository, final CarHibernateService carHibernateService) {
         this.repository = repository;
@@ -22,7 +24,9 @@ public class OrderHibernateService {
     public OrderHibernate create() {
         final OrderHibernate order = new OrderHibernate();
         order.setCars(createListCars(order));
+        logger.debug("Create order with {} cars", order.getCars().size());
         repository.save(order);
+        logger.debug("Save order with id {}", order.getId_order());
         return order;
     }
 
@@ -37,16 +41,28 @@ public class OrderHibernateService {
     }
 
     public Optional<OrderHibernate> getById(final String id) {
-        return repository.getById(id);
+        Optional<OrderHibernate> order = repository.getById(id);
+        if (order.isEmpty() || order == null) {
+            logger.info("Can't find order with id {}", id);
+        }
+        return order;
     }
 
     public List<OrderHibernate> getAll() {
-        return repository.getAll();
+        logger.info("View all orders");
+        List<OrderHibernate> orders = repository.getAll();
+        if (orders.isEmpty() || orders == null){
+            logger.warn("There are no orders in DB");
+        }
+        return orders;
     }
 
     public void delete (final String id){
         if (!(id == null || id.isEmpty())) {
             repository.delete(id);
+            logger.debug("Delete order with id {}", id);
+        } else {
+            logger.warn("Incorrect id for deleting order");
         }
     }
 }
